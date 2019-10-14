262b8e6ce17559c18f2655712a7fb5b18aa665bd
Puneeth Irvichetty <puneeth2011@gmail.com>
2019-10-14 16:35:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,45 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = "${file("${var.credentials}")}"
+  project     = "${var.gcp_project}"
+  region      = "${var.region}"
+}
+
+
+//MERN Stack
+
+//Reserving MERN Stack IP
+resource "google_compute_address" "mernip" {
+  name   = "mernip"
+  region = "${var.mern_stack_instance_ip_region}"
+}
+
+  
+// MERN Stack Instance
+resource "google_compute_instance" "mern-stack" {
+  name         = "${var.mern_stack_instance_name}"
+  machine_type = "${var.mern_stack_machine_type}"
+  zone         = "${var.mern_stack_zone}"
+  
+   tags = ["http-server"]
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network    = "${var.mern_stack_vpc_name}"
+    subnetwork = "${var.mern_stack_subnet_name}"
+
+    access_config {
+      // Ephemeral IP
+
+      nat_ip       = "${google_compute_address.mernip.address}"
+      network_tier = "PREMIUM"
+    }
+  }
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install git  -y; git clone https://github.com/iamdaaniyaal/gcpterraform.git; cd gcpterraform/mern; sudo chmod 777 mern.sh; sh mern.sh"
+
+ 
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "credentials" {}
+variable "gcp_project" {}
+variable "region" {}
+variable "mern_stack_instance_ip_region" {}
+variable "mern_stack_instance_name" {}
+variable "mern_stack_machine_type" {}
+variable "mern_stack_zone" {}
+variable "mern_stack_vpc_name" {}
+variable "mern_stack_subnet_name" {}
