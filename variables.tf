c4ee2bbc19d0a901ef2b81531a24ab2ede5018a3
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-31 17:19:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "credentials" {}
 variable "gcp_project" {}
 variable "region" {}
+variable "mern_stack_instance_ip_name" {}
 variable "mern_stack_instance_ip_region" {}
 variable "mern_stack_instance_name" {}
 variable "mern_stack_machine_type" {}
